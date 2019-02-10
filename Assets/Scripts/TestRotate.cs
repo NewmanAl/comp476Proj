bc65bf9b6fd7167fd0a06e17140f9eb67e57ef65
Alex Newman <newman.alx@gmail.com>
2019-02-10 10:37:06 -0500

    Initial commit with empty unity project

---
+++ Assets/Scripts/TestRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_rotateSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, m_rotateSpeed * Time.deltaTime, Space.World);
+    }
+}

6fdbb8fa5be2fb54c7be8d088732b77d70176aae
GitHubBoss39 <sandnfevr@gmail.com>
2019-02-14 11:35:38 -0500

    Flocking

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager manager;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(manager.minSpeed, manager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlockRules();
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void FlockRules()
+    {
+        GameObject[] crowsHolder;
+        crowsHolder = manager.crows;
+
+        Vector3 avgCenter = Vector3.zero;
+        Vector3 avoidance = Vector3.zero;
+        float avgSpeed = 0.01f;
+        float nDist;
+        int groupSize = 0;
+
+        foreach (GameObject go in crowsHolder)
+        {
+            if (go != this.gameObject)
+            {
+                nDist = Vector3.Distance(go.transform.position, transform.position);
+                if (nDist <= manager.neighborDist)
+                {
+                    avgCenter += go.transform.position;
+                    groupSize++;
+
+                    if (nDist < 1.0f)
+                    {
+                        avoidance += (transform.position - go.transform.position);
+                    }
+
+                    Flock flock = go.GetComponent<Flock>();
+                    avgSpeed += flock.speed;
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            avgCenter /= groupSize;
+            speed = avgSpeed / groupSize;
+
+            Vector3 direction = (avgCenter + avoidance) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), manager.rotSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject crow;
+    public int numCrows = 20;
+    public GameObject[] crows;
+    public Vector3 flyRange = new Vector3(10, 20, 10);
+
+    [Header("Crow Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighborDist;
+    [Range(0.0f, 5.0f)]
+    public float rotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crows = new GameObject[numCrows];
+        for (int i = 0; i < numCrows; i++)
+        {
+            Vector3 position = transform.position + new Vector3(Random.Range(-flyRange.x, flyRange.x),
+                                                                Random.Range(-flyRange.y, flyRange.y),
+                                                                Random.Range(-flyRange.z, flyRange.z));
+            crows[i] = (GameObject)Instantiate(crow, position, Quaternion.identity);
+            crows[i].GetComponent<Flock>().manager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

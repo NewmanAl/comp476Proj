33cb1293a47ed228dd8d08b709cfdf1b13609e56
GitHubBoss39 <sandnfevr@gmail.com>
2019-03-15 19:42:17 -0400

    Flocking birds and panda behavior tree are added

---
+++ Assets/Scripts/AI/EnemyAI/CoverPointManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverPointManager : MonoBehaviour
+{
+    [SerializeField]Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool SeePlayer()
+    {
+        Vector3 distance = player.transform.position - transform.position;
+        RaycastHit hit;
+        bool isWall = false;
+
+        Debug.DrawRay(transform.position, distance, Color.red);
+
+        if (Physics.Raycast(transform.position, distance, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Wall")
+            {
+                isWall = true;
+            }
+        }
+
+        if (!isWall)
+            return true;
+        else
+            return false;
+    }
+}

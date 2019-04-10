06ea8d611ef4d1065f81b9a14247160004d87388
Alex Newman <newman.alx@gmail.com>
2019-04-09 20:35:24 -0400

    Added sounds to Zombies: -Footsteps -Attack sound -Periodic moaning

--- Assets/Scripts/Player/AnimationTriggers.cs
+++ Assets/Scripts/Player/AnimationTriggers.cs
@@ -9,8 +9,8 @@ public class AnimationTriggers : MonoBehaviour
     private AudioSource m_audioSource;
 
     private Animator m_animator;
-
     private SoundTrigger m_soundTrigger;
+    private Player m_player;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,7 @@ public class AnimationTriggers : MonoBehaviour
         m_audioSource = gameObject.GetComponent<AudioSource>();
         m_animator = gameObject.GetComponent<Animator>();
         m_soundTrigger = GameObject.Find("PlayerHandle").transform.Find("SoundTrigger").GetComponent<SoundTrigger>();
+        m_player = GameObject.Find("PlayerHandle").GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -29,23 +30,26 @@ public class AnimationTriggers : MonoBehaviour
     // Triggered by the animator every time the player makes a footstep
     private void Step()
     {
-        bool isRunning = m_animator.GetBool("IsRunning");
-        bool isCrouching = m_animator.GetBool("IsCrouch");
-        if(isRunning)
+        if(m_player.inputEnabled)
         {
-            m_audioSource.volume = 1;
-            m_soundTrigger.AddSound(10f);
+            bool isRunning = m_animator.GetBool("IsRunning");
+            bool isCrouching = m_animator.GetBool("IsCrouch");
+            if(isRunning)
+            {
+                m_audioSource.volume = 1;
+                m_soundTrigger.AddSound(10f);
+            }
+            else if (isCrouching)
+            {
+                m_audioSource.volume = 0.25f;
+                m_soundTrigger.AddSound(2f);
+            }
+            else
+            {
+                m_audioSource.volume = 0.5f;
+                m_soundTrigger.AddSound(5f);
+            }
+            m_audioSource.PlayOneShot(m_walkDirtClips[Random.Range(0, m_walkDirtClips.Length)]);
         }
-        else if (isCrouching)
-        {
-            m_audioSource.volume = 0.25f;
-            m_soundTrigger.AddSound(2f);
-        }
-        else
-        {
-            m_audioSource.volume = 0.5f;
-            m_soundTrigger.AddSound(5f);
-        }
-        m_audioSource.PlayOneShot(m_walkDirtClips[Random.Range(0, m_walkDirtClips.Length)]);
     }
 }

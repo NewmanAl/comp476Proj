68a21e79ef137b70b0ecb86f21c775fc31b12af0
Simon Ning <ningshihao9478@gmail.com>
2019-04-11 01:43:21 -0400

    Added Day and Night Cycle

---
+++ Assets/Scripts/EventTrigger/DayAndNight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    public Transform sun;
+    public int rotationScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationScale = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sun.Rotate(rotationScale * Time.deltaTime, 0, 0);
+    }
+}
